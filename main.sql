014dcd0ebefcb26e2ae95cfe9e77e7f6e4d24719
runfourest <aefournier50@gmail.com>
2019-10-30 14:13:35 -0400

    wednesday 30

--- main.sql
+++ main.sql
@@ -263,29 +263,35 @@ SELECT
 	A.AlergyCount,
 	Person.Person_id,
 	Person.birthdate,
-	Person.deathdate,
+	Person.death_date,
 	Person.gender,
 
 	CASE 
 		WHEN
-		Person.birthdate <= Person.deathdate 
-			THEN 
+		Person.birthdate >= Person.death_date 
+		THEN 
 				'Alive'
-		Else
-			'Dead'
-		End as isDead,
-	Person
+		ELSE
+				'Dead'
+		End as isDead
+	
 FROM
-	Patient as Patient
+	openmrs.Patient as Patient
+INNER JOIN openmrs.patient_identifier as pid on pid.patient_id = patient.patient_id
+INNER JOIN openmrs.person as person on person.person_id = pid.uuid
 INNER JOIN ( 
 	SELECT
 		COUNT(Allergy.Allergy_ID) as AlergyCount,
 		PATIENT.PATIENT_ID
 	FROM
-		PATIENT as Patient
-		INNER JOIN Allergy as Allergy on Allergy.Patient_ID = Patient.Patient_ID
+		openmrs.PATIENT as Patient
+		INNER JOIN openmrs.Allergy as Allergy on Allergy.Patient_ID = Patient.Patient_ID
 	GROUP BY PATIENT.PATIENT_ID
 ) as A on A.patient_id = patient.patient_id 
+
+
+
+
 
 
 
@@ -367,6 +373,19 @@ SELECT
 *
 INTO #TEMP3
 FROM 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
